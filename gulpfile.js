48d9a9f6bd19b514295e77939ce52fd0ebff6933
Matt Lewis <domattthings@gmail.com>
2018-04-01 13:36:26 -0400

    Clean up About Me page

--- gulpfile.js
+++ gulpfile.js
@@ -98,3 +98,5 @@ gulp.task('watch', function () {
  * compile the jekyll site, launch BrowserSync & watch files.
  */
 gulp.task('default', ['js', 'stylus', 'browser-sync', 'watch']);
+
+gulp.task('build', ['js', 'stylus', 'imagemin']);
